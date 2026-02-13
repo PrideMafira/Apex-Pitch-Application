fcd8bcb35ad6149b4985c34eb43773d4117f149b
Pride Mafira <tadiwanashepride50@gmail.com>
2026-02-13 14:01:05 +0200

    Initial Commit

---
+++ Apex Pitch Application/Apex_Pitch_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apex_Pitch_ApplicationApp.swift
+//  Apex Pitch Application
+//
+//  Created by Pride Mafira  on 13/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct Apex_Pitch_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apex Pitch Application/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apex Pitch Application
+//
+//  Created by Pride Mafira  on 13/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
