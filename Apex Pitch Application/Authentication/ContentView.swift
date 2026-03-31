d5b9a636c2856a3e27b55f0ca1fbd66b92248f74
Pride Mafira <tadiwanashepride50@gmail.com>
2026-03-31 09:35:35 +0200

    put folders and files in the right place

---
+++ Apex Pitch Application/Authentication/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Apex Pitch Application
+//
+//  Created by Pride Mafira  on 13/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // Stores the app's auth state.
+    @StateObject private var authViewModel = AuthViewModel()
+    
+    var body: some View {
+        Group {
+            // Show the home page when the user is signed in.
+            if authViewModel.isAuthenticated {
+                HomePage(authViewModel: authViewModel)
+            } else {
+                // Show the welcome page when the user is signed out.
+                WelcomePage(authViewModel: authViewModel)
+            }
+        }
+        .task {
+             // Restore any existing user session on launch.
+            await authViewModel.getInitialSession()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
